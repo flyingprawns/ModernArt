82f7fff70b84638c4d4e8b5b766048d532701844
Qijie Zhu <qzhu9@ucsc.edu>
2021-09-06 18:00:17 -0700

    working on draw_spot_painting()

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import colorgram
+from turtle import Turtle, Screen
 
 
 def extract_colors(source, count):
@@ -9,7 +10,7 @@ def extract_colors(source, count):
     :return: []
     """
     extracted_colors = colorgram.extract(source, count)
-    colors = []
+    final_colors = []
     for color in extracted_colors:
         # Reformat the current color as a tuple (r, g, b)
         rgb = color.rgb
@@ -18,9 +19,43 @@ def extract_colors(source, count):
         b = rgb[2]
         rgb = (r, g, b)
         # Add the reformatted color to 'colors'
-        colors.append(rgb)
-    return colors
+        final_colors.append(rgb)
+    return final_colors
 
+
+def draw_spot_painting(turtle, color_bank, row_count=10, dots_per_row=10, dot_size=25, dot_spacing=50):
+    """
+    Draws a 10x10 spot painting with dots of size 25, set 50 paces apart.
+    Color will be randomly selected from color_bank.
+    """
+    # Calculate starting coordinate
+    painting_width = dots_per_row * (dot_size + dot_spacing)
+    painting_height = row_count * (dot_size + dot_spacing)
+    starting_x = painting_width/-2.0
+    starting_y = painting_height/2.0
+    # Iterate through every coordinate and draw a dot:
+    curr_x = starting_x
+    curr_y = starting_y
+    turtle.penup()
+    for row in range(row_count):
+        for dot in range(dots_per_row):
+            turtle.dot(dot_size, 'black')
+            turtle.setposition(curr_x, curr_y)
+            curr_x += (dot_size + dot_spacing)
 
 colors = extract_colors('img.jpg', 10)
 print(colors)
+
+# Create turtle and screen
+timmy = Turtle()
+my_screen = timmy.getscreen()
+
+# Adjust turtle/screen settings
+my_screen.colormode(255)
+timmy.speed(0)
+
+# Draw painting
+draw_spot_painting(timmy, colors)
+
+# Exit program on click
+my_screen.exitonclick()
